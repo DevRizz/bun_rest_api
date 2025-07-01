225f38a1723cbe1bfc2f56de9ec070535ea115df
DevRizz <devruntimeerror69@gmail.com>
2025-07-01 23:12:56 +0530

    added pluggins support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { Elysia } from "elysia";
+import { pluggin } from "./pluggin"; // Importing the plugin
 
+//Pluggin, we can divide our application into multiple plugins like for spotify we can have pluggin for artists, tracks, albums, etc.
+
+//WE moved the pluggin code to a separate file src/pluggin.ts
+/* const pluggin = new Elysia()
+  .state("pluggin_Version", "5.8.9") // Setting a state variable for the plugin
+  .get("/plugin", () => "This is a plugin route")
+  .get("/plugin/about", () => "This is a plugin about route") */
+
+//Application
 const app = new Elysia()
   .get("/", () => "Hello Elysia and this is used to build REST API similarly to how we used express in Node JS")
+  .use(pluggin) // Using the plugin
   .state({
     description: "API Version",
     type: "string",
@@ -21,6 +32,9 @@ const app = new Elysia()
   .get("/version", ({ store, getDate}) => {
     console.log("Current Date:", getDate());
     console.log("API version:", JSON.stringify(store));
+
+    //calling pluggin state variable
+    console.log("Pluggin Version:", store.pluggin_Version);
     return `API Version: ${store.example}, Current Date: ${getDate()}`;
   })
   .get("/api/data", () => {
